9d6d23b6d850e14982247d52b51ffb7e45b321fa
Rishi Soni <rishisoni6071@gmail.com>
2021-03-19 14:37:58 +0530

    Added Google Sign-in in Build folder

--- build/js/login.js
+++ build/js/login.js
@@ -1,3 +1,18 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert('User signed out.');
+    });
+}
+
 // It's a Self-Invoking Function to Initialise Firebase Auth UI
 (() => {
     // Initialize the FirebaseUI Widget using Firebase.
