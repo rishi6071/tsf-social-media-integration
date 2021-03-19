5d9d556199d353229da03ae88a7ece9c32be528f
Rishi Soni <rishisoni6071@gmail.com>
2021-03-20 03:42:07 +0530

    Google Sign-in Completely Done

--- build/js/index.js
+++ build/js/index.js
@@ -9,15 +9,15 @@
 })()
 
 // Animation Changing according to width
-const checkWidthDoAnime = () => {
-    // console.log(window.outerWidth);
-    if (window.outerWidth <= 768) {
-        const dataAos = document.body.querySelectorAll('[data-aos]');
-        for (let i = 0; i < dataAos.length; i++) {
-            dataAos[i].setAttribute('data-aos', 'fade-up');
-        }
-    }
-}
+// const checkWidthDoAnime = () => {
+//     // console.log(window.outerWidth);
+//     if (window.outerWidth <= 768) {
+//         const dataAos = document.body.querySelectorAll('[data-aos]');
+//         for (let i = 0; i < dataAos.length; i++) {
+//             dataAos[i].setAttribute('data-aos', 'fade-up');
+//         }
+//     }
+// }
 
 // Contact Form
 const contactUs = (event) => {
