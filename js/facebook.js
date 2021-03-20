fd4e603d8453b24c35ef84f47b87204462ce5804
Rishi Soni <rishisoni6071@gmail.com>
2021-03-20 22:33:42 +0530

    Bug Fixing

--- js/facebook.js
+++ js/facebook.js
@@ -1,3 +1,17 @@
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '346674706727132',
+        cookie: true,                     // Enable cookies to allow the server to access the session.
+        xfbml: true,                     // Parse social plugins on this webpage.
+        version: 'v10.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+        statusChangeCallback(response);        // Returns the login status.
+    });
+};
+
 function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
     if (response.status === 'connected') {   // Logged into your webpage and Facebook.
         testAPI();
@@ -19,21 +33,6 @@ function checkLoginState() {               // Called when a person is finished w
         statusChangeCallback(response);
     });
 }
-
-
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '346674706727132',
-        cookie: true,                     // Enable cookies to allow the server to access the session.
-        xfbml: true,                     // Parse social plugins on this webpage.
-        version: 'v10.0'           // Use this Graph API version for this call.
-    });
-
-
-    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
-        statusChangeCallback(response);        // Returns the login status.
-    });
-};
 
 // When User Clicks on Log in With Facebook Button
 const loginWithFacebook = () => {
@@ -61,7 +60,7 @@ const logoutFromFacebook = () => {
             text: `User is Logged Out`,
             icon: 'success'
         });
-        
+
         document.querySelector('#logout_fb').style.display = "none";
         document.querySelector('#signin_user').style.display = "block";
         document.querySelector('#user_info').style.display = "block";
