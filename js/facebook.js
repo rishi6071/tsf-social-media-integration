266a693e3ba5833057ce499fd5fe41b98afc7589
Rishi Soni <rishisoni6071@gmail.com>
2021-03-20 15:46:01 +0530

    Added FB Login Code and Checking

--- js/facebook.js
+++ js/facebook.js
@@ -1,16 +1,41 @@
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+    }
+}
 
-//     <script>
-//     // Google Sign-out Method
-//     function signOut() {
-//         var auth2 = gapi.auth2.getAuthInstance();
-//         auth2.signOut().then(function () {
-//             alert('User signed out.');
-//             document.querySelector('#user_info').style.display = "none";
-//             document.querySelector('#signin_user').style.display = "block";
-//             document.querySelector('#home_profile_icon').src = "./img/icon/profile.png";
-//             document.querySelector('#home_profile_icon').alt = "Profile Icon";
-//             document.querySelector('#user_img').src = "./img/icon/profile.png";
-//             document.querySelector('#user_img').alt = "Profile Icon";
-//         });
-//     }
-// </script>
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {   // See the onlogin handler
+        statusChangeCallback(response);
+    });
+}
+
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '346674706727132',
+        cookie: true,                     // Enable cookies to allow the server to access the session.
+        xfbml: true,                     // Parse social plugins on this webpage.
+        version: 'v10.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+        statusChangeCallback(response);        // Returns the login status.
+    });
+};
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
